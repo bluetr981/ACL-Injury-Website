5f1ea2c1abeb3a9dc12c356c54780710a6e43d0d
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-07-24 21:30:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,53 +32,53 @@ def home():
           session["LAST_REQUEST"] = LAST_REQUEST
 
           match session["SELECTED_MODEL"]:
-              case "Model #1" | "Model #2" | "Model #5":
-                  cts = request.form["CoronalTibialSlope"]
-                  mts = request.form["MedialTibialSlope"]
-                  lts = request.form["LateralTibialSlope"]
-                  mtd = request.form["MedialTibialDepth"]
-                  sex = request.form["selectsex"]
+              case "RF_Acc_[1, 2, 3, 4, 5]" | "SVM_Acc_model_[1, 2, 3, 4, 5]" | "XGB_F2_model_[1, 2, 3, 4, 5]":
+                cts = request.form["CoronalTibialSlope"]
+                mts = request.form["MedialTibialSlope"]
+                lts = request.form["LateralTibialSlope"]
+                mtd = request.form["MedialTibialDepth"]
+                sex = request.form["selectsex"]
+                
+                session["CTS"] = cts
+                session["MTS"] = mts
+                session["LTS"] = lts
+                session["MTD"] = mtd
+                session["SEX"] = sex
                   
-                  session["CTS"] = cts
-                  session["MTS"] = mts
-                  session["LTS"] = lts
-                  session["MTD"] = mtd
-                  session["SEX"] = sex
+                path = f'models/trained_RF_Acc_model_[1, 2, 3, 4, 5]_1.joblib'
+                input_array = np.array([cts, mts, lts, mtd, replacement_rules_feature[sex]]).reshape(1, -1)
+
+                return redirect(url_for("home", REQUEST_HISTORY=session.get("REQUEST_HISTORY", []), SELECTED_MODEL = session["SELECTED_MODEL"], MODEL_CHOSEN = True, CTS=cts, MTS=mts, LTS=lts, MTD=mtd, SEX=sex, SHOW_RESULT=True, RESULT=perform_inference(path, input_array)))
+
+              case "SVM_Acc_model_[1, 2, 4, 5]":
+                cts = request.form["CoronalTibialSlope"]
+                mts = request.form["MedialTibialSlope"]
+                mtd = request.form["MedialTibialDepth"]
+                sex = request.form["selectsex"]
                   
-                  path = f'models/trained_RF_Acc_model_[1, 2, 3, 4, 5]_1.joblib'
-                  input_array = np.array([cts, mts, lts, mtd, replacement_rules_feature[sex]]).reshape(1, -1)
-
-                  return redirect(url_for("home", REQUEST_HISTORY=session.get("REQUEST_HISTORY", []), SELECTED_MODEL = session["SELECTED_MODEL"], MODEL_CHOSEN = True, CTS=cts, MTS=mts, LTS=lts, MTD=mtd, SEX=sex, SHOW_RESULT=True, RESULT=perform_inference(path, input_array)))
-
-              case "Model #3":
-                  cts = request.form["CoronalTibialSlope"]
-                  mts = request.form["MedialTibialSlope"]
-                  mtd = request.form["MedialTibialDepth"]
-                  sex = request.form["selectsex"]
+                session["CTS"] = cts
+                session["MTS"] = mts
+                session["MTD"] = mtd
+                session["SEX"] = sex
                   
-                  session["CTS"] = cts
-                  session["MTS"] = mts
-                  session["MTD"] = mtd
-                  session["SEX"] = sex
+                path = f'models/trained_SVM_Acc_model_[1, 2, 4, 5]_1.joblib'
+                input_array = np.array([cts, mts, mtd, replacement_rules_feature[sex]]).reshape(1, -1)
+
+                return redirect(url_for("home", REQUEST_HISTORY=session.get("REQUEST_HISTORY", []), SELECTED_MODEL = session["SELECTED_MODEL"], MODEL_CHOSEN = True, CTS=cts, MTS=mts, MTD=mtd, SEX=sex, SHOW_RESULT=True, RESULT=perform_inference(path, input_array)))
+
+              case "SVM_F2_model_[2, 3, 4]":
+                mts = request.form["MedialTibialSlope"]
+                lts = request.form["LateralTibialSlope"]
+                mtd = request.form["MedialTibialDepth"]
+
+                session["MTS"] = mts
+                session["LTS"] = lts
+                session["MTD"] = mtd
                   
-                  path = f'models/trained_SVM_Acc_model_[1, 2, 4, 5]_1.joblib'
-                  input_array = np.array([cts, mts, mtd, replacement_rules_feature[sex]]).reshape(1, -1)
+                path = f'models/trained_SVM_Fb_model_[2, 3, 4]_1.joblib'
+                input_array = np.array([mts, lts, mtd]).reshape(1, -1)
 
-                  return redirect(url_for("home", REQUEST_HISTORY=session.get("REQUEST_HISTORY", []), SELECTED_MODEL = session["SELECTED_MODEL"], MODEL_CHOSEN = True, CTS=cts, MTS=mts, MTD=mtd, SEX=sex, SHOW_RESULT=True, RESULT=perform_inference(path, input_array)))
-
-              case "Model #4":
-                  mts = request.form["MedialTibialSlope"]
-                  lts = request.form["LateralTibialSlope"]
-                  mtd = request.form["MedialTibialDepth"]
-
-                  session["MTS"] = mts
-                  session["LTS"] = lts
-                  session["MTD"] = mtd
-                  
-                  path = f'models/trained_SVM_Fb_model_[2, 3, 4]_1.joblib'
-                  input_array = np.array([mts, lts, mtd]).reshape(1, -1)
-
-                  return redirect(url_for("home", REQUEST_HISTORY = session.get("REQUEST_HISTORY", []), SELECTED_MODEL = session["SELECTED_MODEL"], MODEL_CHOSEN = True, MTS=mts, LTS=lts, MTD=mtd, SHOW_RESULT=True, RESULT=perform_inference(path, input_array)))
+                return redirect(url_for("home", REQUEST_HISTORY = session.get("REQUEST_HISTORY", []), SELECTED_MODEL = session["SELECTED_MODEL"], MODEL_CHOSEN = True, MTS=mts, LTS=lts, MTD=mtd, SHOW_RESULT=True, RESULT=perform_inference(path, input_array)))
         elif session["LAST_REQUEST"] == "POST":
             REQUEST_HISTORY.append("POST")
             LAST_REQUEST = "POST"
@@ -88,18 +88,18 @@ def home():
 
             return redirect(url_for("home", REQUEST_HISTORY = session.get("REQUEST_HISTORY", []), MODEL_CHOSEN=False, SHOW_RESULT=False, RESULT=-1))
         else:
-           selectedmodel = request.form["select_model"]
-
-           REQUEST_HISTORY.append("POST")
-           LAST_REQUEST = "POST"
-
-           session["SELECTED_MODEL"] = selectedmodel
-           session["REQUEST_HISTORY"] = REQUEST_HISTORY
-           session["LAST_REQUEST"] = LAST_REQUEST
-
-           return render_template("index.html", REQUEST_HISTORY = session.get("REQUEST_HISTORY", []), MODEL_CHOSEN = True, SELECTED_MODEL=session["SELECTED_MODEL"])
+            selectedmodel = request.form["select_model"]
+            
+            REQUEST_HISTORY.append("POST")
+            LAST_REQUEST = "POST"
+            
+            session["SELECTED_MODEL"] = selectedmodel
+            session["REQUEST_HISTORY"] = REQUEST_HISTORY
+            session["LAST_REQUEST"] = LAST_REQUEST
+            
+            return render_template("index.html", REQUEST_HISTORY = session.get("REQUEST_HISTORY", []), MODEL_CHOSEN = True, SELECTED_MODEL=session["SELECTED_MODEL"])
     else:
-       if ("SELECTED_MODEL" in session and "SELECTED_MODEL" in request.args and session["LAST_REQUEST"] != "GET"):
+        if ("SELECTED_MODEL" in session and "SELECTED_MODEL" in request.args and session["LAST_REQUEST"] != "GET"):
           REQUEST_HISTORY.append("GET")
           LAST_REQUEST = "GET"
 
@@ -107,7 +107,7 @@ def home():
           session["LAST_REQUEST"] = LAST_REQUEST
 
           match session["SELECTED_MODEL"]:
-              case "Model #1" | "Model #2" | "Model #5":
+              case "RF_Acc_[1, 2, 3, 4, 5]" | "SVM_Acc_model_[1, 2, 3, 4, 5]" | "XGB_F2_model_[1, 2, 3, 4, 5]":
                   session.pop("CTS", None)
                   session.pop("MTS", None)
                   session.pop("LTS", None)
@@ -116,7 +116,7 @@ def home():
 
                   return render_template("index.html", REQUEST_HISTORY=session.get("REQUEST_HISTORY", []), SELECTED_MODEL = request.args.get("SELECTED_MODEL"), MODEL_CHOSEN = True, cts = request.args.get("CTS"), mts = request.args.get("MTS"), lts = request.args.get("LTS"), mtd = request.args.get("MTD"), sex = request.args.get("SEX"), RESULT=request.args.get("RESULT"), SHOW_RESULT=True)
 
-              case "Model #3":
+              case "SVM_Acc_model_[1, 2, 4, 5]":
                   session.pop("CTS", None)
                   session.pop("MTS", None)
                   session.pop("MTD", None)
@@ -130,11 +130,11 @@ def home():
                   session.pop("MTD", None)
 
                   return render_template("index.html", REQUEST_HISTORY=session.get("REQUEST_HISTORY", []), SELECTED_MODEL = request.args.get("SELECTED_MODEL"), MODEL_CHOSEN = True, mts = request.args.get("MTS"), lts = request.args.get("LTS"), mtd = request.args.get("MTD"), RESULT=request.args.get("RESULT"), SHOW_RESULT=True)
-       elif ("SELECTED_MODEL" in request.args and session["LAST_REQUEST"] == "GET"):
+        elif ("SELECTED_MODEL" in request.args and session["LAST_REQUEST"] == "GET"):
            return render_template("index.html", REQUEST_HISTORY = session.get("REQUEST_HISTORY", []), MODEL_CHOSEN=False, SHOW_RESULT=False, RESULT=-1)
-       elif (len(REQUEST_HISTORY) > 3 and REQUEST_HISTORY[-1] == "GET" or len(REQUEST_HISTORY) >= 2 and len(REQUEST_HISTORY) <= 3 and session["LAST_REQUEST"] == "GET"):
+        elif (len(REQUEST_HISTORY) > 3 and REQUEST_HISTORY[-1] == "GET" or len(REQUEST_HISTORY) >= 2 and len(REQUEST_HISTORY) <= 3 and session["LAST_REQUEST"] == "GET"):
            return render_template("index.html", REQUEST_HISTORY = session.get("REQUEST_HISTORY", []), MODEL_CHOSEN=False, SHOW_RESULT=False, RESULT=-1)
-       else:
+        else:
            REQUEST_HISTORY.append("GET")
            LAST_REQUEST = "GET"
 
@@ -144,7 +144,6 @@ def home():
            if ("SELECTED_MODEL" in session):
                return render_template("index.html", SELECTED_MODEL = session["SELECTED_MODEL"], REQUEST_HISTORY = session.get("REQUEST_HISTORY", []), MODEL_CHOSEN=False, SHOW_RESULT=False, RESULT=-1)
            else:
-               session.pop("SELECTED_MODEL", None)
                return render_template("index.html", REQUEST_HISTORY = session.get("REQUEST_HISTORY", []), MODEL_CHOSEN=False, SHOW_RESULT=False, RESULT=-1)
                
 def perform_inference(model_path:str, array:np.array) -> np.array:
