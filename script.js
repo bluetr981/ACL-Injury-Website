5c4a51de87b964ee8c959a1cf4f2d5688be65096
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-08-04 21:57:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function returnMenuStatus() {
   const selectionMenuStatus = document.getElementById("selmod").value;
   document.getElementById("demo").innerHTML = "You have selected " + selectionMenuStatus;
   sessionStorage("selected_model", selectionMenuStatus);
+  document.getElementById("demo1").innerHTML = "CURRENT SELECTION: " + sessionStorage.getItem("selected_model");
 }
