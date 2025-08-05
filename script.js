19e4ed656c4771e95826e1b5b106f2b941adc235
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-08-05 15:47:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let selectionMenuStatus = document.getElementById("selmod").value;
+sessionStorage.setItem("selected_model", "RF_Acc_[1, 2, 3, 4, 5]");
 
 function returnMenuStatus() {
+  let selectionMenuStatus = document.getElementById("selmod").value;
+
+  sessionStorage.clear();
   sessionStorage.setItem("selected_model", selectionMenuStatus);
   document.getElementById("demo").innerHTML = "CURRENT MODEL: " + sessionStorage.getItem("selected_model");
 }
