c0722e22dc6b40464f7deb98c92eaf490f61b717
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-08-05 14:43:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ let selectionMenuStatus = document.getElementById("selmod").value;
 
 function returnMenuStatus() {
   sessionStorage.setItem("selected_model", selectionMenuStatus);
+  document.getElementById("demo").innerHTML = "CURRENT MODEL: " + sessionStorage.getItem("selected_model");
 }
