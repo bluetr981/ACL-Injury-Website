b8089fe51ce7823c5ee46d0f18f34b6decdb719e
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-08-06 14:01:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
+sessionStorage.setItem("CTS-degrees", document.getElementById("CTS").value);
+sessionStorage.setItem("MTS-degrees", document.getElementById("MTS").value);
+sessionStorage.setItem("LTS-degrees", document.getElementById("LTS").value);
+sessionStorage.setItem("MTD-degrees", document.getElementById("MTD").value);
+sessionStorage.setItem("selected-sex", document.getElementById("selsex").value);
+
 function returnMenuStatus() {
   let selectionMenuStatus = document.getElementById("selmod").value;
   sessionStorage.setItem("selected_model", selectionMenuStatus);
   location.reload();
+}
+
+function updateCoronalTibialSlope() {
+  sessionStorage.removeItem("CTS-degrees");
+  sessionStorage.setItem("CTS-degrees", document.getElementById("CTS").value);
+}
+
+function updateMedialTibialSlope() {
+  sessionStorage.removeItem("MTS-degrees");
+  sessionStorage.setItem("MTS-degrees", document.getElementById("MTS").value);
+}
+
+function updateLateralTibialSlope() {
+  sessionStorage.removeItem("LTS-degrees");
+  sessionStorage.setItem("LTS-degrees", document.getElementById("LTS").value);
+}
+
+function updateMedialTibialDepth() {
+  sessionStorage.removeItem("MTD-degrees");
+  sessionStorage.setItem("MTD-degrees", document.getElementById("MTD").value);
+}
+
+function updateSelectedSex() {
+  sessionStorage.removeItem("selected-sex");
+  sessionStorage.setItem("selected-sex", document.getElementById("selsex").value);
 }
