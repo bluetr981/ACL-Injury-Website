2109429ebff31116b1361d5aadc684e67c89e747
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-08-19 00:17:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function returnMenuStatus() {
+  selectionMenuStatus = document.getElementById("selmod").value;
+  sessionStorage.setItem("selected_model", selectionMenuStatus);
+  
   if (sessionStorage.getItem("CTS-degrees") != null) {
     sessionStorage.removeItem("CTS-degrees");
   }
