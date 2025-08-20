0910a87b84023353bbc2feb4fed32b0e023170bd
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-08-19 22:51:49 -0400

    Update testapi.js

--- testapi.js
+++ testapi.js
@@ -4,11 +4,11 @@ async function TestAPI(selected_model, CTS, MTS, LTS, MTD, selected_sex) {
         headers: {'Content-Type':'application/json'},
 
         body: JSON.stringify({"selected-model": selected_model,
-                              "CoronalTibialSlope": CTS,
-                              "MedialTibialSlope": MTS,
-                              "LateralTibialSlope": LTS,
-                              "MedialTibialDepth": MTD,
-                              "selected-sex": selected})
+                              "CoronalTibialSlope": String(CTS).replace(null, "0"),
+                              "MedialTibialSlope": String(MTS).replace(null, "0"),
+                              "LateralTibialSlope": String(LTS).replace(null, "0"),
+                              "MedialTibialDepth": String(MTD).replace(null, "0"),
+                              "selected-sex": String(selected_sex).replace(null, "0")
     })
     .then(response => response.json())
     .then(data => console.log(data));
