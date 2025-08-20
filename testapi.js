6f335afe26b05db31371bd0ade742432f00e3039
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-08-19 23:21:13 -0400

    Update testapi.js

--- testapi.js
+++ testapi.js
@@ -1,16 +1,17 @@
-async function TestAPI(selected_model, CTS, MTS, LTS, MTD, selected_sex) {
-    var response = fetch('https://acl-frameworkapitesting.onrender.com/healthz', {
+async function TestAPI() {
+    var response = await fetch('https://acl-frameworkapitesting.onrender.com/healthz', {
         method: "POST",
         headers: {'Content-Type':'application/json'},
 
-        body: JSON.stringify({"selected-model": selected_model,
-                              "CoronalTibialSlope": String(CTS).replace(null, "0"),
-                              "MedialTibialSlope": String(MTS).replace(null, "0"),
-                              "LateralTibialSlope": String(LTS).replace(null, "0"),
-                              "MedialTibialDepth": String(MTD).replace(null, "0"),
-                              "selected-sex": String(selected_sex).replace(null, "0")})
+        body: JSON.stringify({"selected-model": "models/trained_RF_Acc_model_[1, 2, 3, 4, 5]_1.joblib",
+                              "CoronalTibialSlope": "2",
+                              "LateralTibialSlope": "12",
+                              "MedialTibialDepth": "0",
+                              "selected-sex": "Female",
+                              "MedialTibialSlope": "7"}
     })
     .then(response => response.json())
     .then(data => console.log(data));
+    
     location.reload();
 }
