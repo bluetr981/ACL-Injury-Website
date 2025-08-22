60f409aadba2c52b67363e268a8088f84766f6f8
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-08-21 22:55:29 -0400

    Update apifunc.js

--- apifunc.js
+++ apifunc.js
@@ -12,5 +12,6 @@ async function TestAPI() {
     .then(response => response.json())
     .then(data => sessionStorage.setItem("RESULT", data.Prediction));
 
+    location.reload();
     location.replace("https://bluetr981.github.io/ACL-Injury-Website/#result");
 }
