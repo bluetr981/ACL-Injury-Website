20f88301c346de4f311dc02ddadf2cd03d0e059b
Puneet Bhat <152762210+bluetr981@users.noreply.github.com>
2025-08-22 14:09:09 -0400

    Update apifunc.js

--- apifunc.js
+++ apifunc.js
@@ -16,11 +16,11 @@ async function TestAPI() {
         method: "POST",
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify({"selected-model": sessionStorage.getItem("selected_model"),
-                              "CoronalTibialSlope": String(sessionStorage.getItem("CTS-degrees")).replace(null, "-1"),
-                              "MedialTibialSlope": String(sessionStorage.getItem("MTS-degrees")).replace(null, "-1"), 
-                              "LateralTibialSlope": String(sessionStorage.getItem("LTS-degrees")).replace(null, "-1"),
-                              "MedialTibialDepth": String(sessionStorage.getItem("MTD-degrees")).replace(null, "-1"),
-                              "selected-sex": String(sessionStorage.getItem("selected-sex")).replace(null, "-1")})
+                              "CoronalTibialSlope": sessionStorage.getItem("CTS-degrees"),
+                              "MedialTibialSlope": sessionStorage.getItem("MTS-degrees"), 
+                              "LateralTibialSlope": sessionStorage.getItem("LTS-degrees"),
+                              "MedialTibialDepth": sessionStorage.getItem("MTD-degrees"),
+                              "selected-sex": sessionStorage.getItem("selected-sex")})
     })
         
     .then(response => response.json())
